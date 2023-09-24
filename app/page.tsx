1d8db520c36f92787cd75c01a0b6796d0a1be382
Re-he4i5 <kuronekoryohei@gmail.com>
2023-09-24 23:55:03 +0900

    トップページの仮レイアウト

--- app/page.tsx
+++ app/page.tsx
@@ -1,114 +1,214 @@
 import Image from "next/image";
 import Link from "next/link";
+import Sidebar from "./components/Sidebar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 pb-5">
+        <div className="lg:col-span-2">
+          <div className="flex justify-center lg:justify-center">
+            <h2>Welcome to ReCodeYoiko</h2>
+          </div>
+
+          <div className="bg-white py-6 sm:py-8 lg:py-12">
+            <div className="mx-auto max-w-screen-xl px-4 md:px-8">
+              <div className="mb-10 md:mb-16">
+                <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">
+                  Blog
+                </h2>
+
+                <p className="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg">
+                  This is a section of some simple filler text, also known as
+                  placeholder text. It shares some characteristics of a real
+                  written text but is random or otherwise generated.
+                </p>
+              </div>
+
+              <div className="grid gap-4 sm:grid-cols-2 md:gap-6 lg:grid-cols-2 xl:grid-cols-2 xl:gap-8">
+                <div className="flex flex-col items-center overflow-hidden rounded-lg border md:flex-row">
+                  <a
+                    href="#"
+                    className="group relative block h-48 w-full shrink-0 self-start overflow-hidden bg-gray-100 md:h-full md:w-32 lg:w-48"
+                  >
+                    <img
+                      src="https://images.unsplash.com/photo-1593508512255-86ab42a8e620?auto=format&q=75&fit=crop&w=600"
+                      loading="lazy"
+                      alt="Photo by Minh Pham"
+                      className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+                    />
+                  </a>
+
+                  <div className="flex flex-col gap-2 p-4 lg:p-6">
+                    <span className="text-sm text-gray-400">July 19, 2021</span>
+
+                    <h2 className="text-xl font-bold text-gray-800">
+                      <a
+                        href="#"
+                        className="transition duration-100 hover:text-indigo-500 active:text-indigo-600"
+                      >
+                        New trends in Tech
+                      </a>
+                    </h2>
+
+                    <p className="text-gray-500">
+                      This is a section of some simple filler text, also known
+                      as placeholder text.
+                    </p>
+
+                    <div>
+                      <a
+                        href="#"
+                        className="font-semibold text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700"
+                      >
+                        Read more
+                      </a>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="flex flex-col items-center overflow-hidden rounded-lg border md:flex-row">
+                  <a
+                    href="#"
+                    className="group relative block h-48 w-full shrink-0 self-start overflow-hidden bg-gray-100 md:h-full md:w-32 lg:w-48"
+                  >
+                    <img
+                      src="https://images.unsplash.com/photo-1550745165-9bc0b252726f?auto=format&q=75&fit=crop&w=600"
+                      loading="lazy"
+                      alt="Photo by Lorenzo Herrera"
+                      className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+                    />
+                  </a>
+
+                  <div className="flex flex-col gap-2 p-4 lg:p-6">
+                    <span className="text-sm text-gray-400">
+                      April 07, 2021
+                    </span>
+
+                    <h2 className="text-xl font-bold text-gray-800">
+                      <a
+                        href="#"
+                        className="transition duration-100 hover:text-indigo-500 active:text-indigo-600"
+                      >
+                        Working with legacy stacks
+                      </a>
+                    </h2>
+
+                    <p className="text-gray-500">
+                      This is a section of some simple filler text, also known
+                      as placeholder text.
+                    </p>
+
+                    <div>
+                      <a
+                        href="#"
+                        className="font-semibold text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700"
+                      >
+                        Read more
+                      </a>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="flex flex-col items-center overflow-hidden rounded-lg border md:flex-row">
+                  <a
+                    href="#"
+                    className="group relative block h-48 w-full shrink-0 self-start overflow-hidden bg-gray-100 md:h-full md:w-32 lg:w-48"
+                  >
+                    <img
+                      src="https://images.unsplash.com/photo-1542759564-7ccbb6ac450a?auto=format&q=75&fit=crop&w=600"
+                      loading="lazy"
+                      alt="Photo by Magicle"
+                      className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+                    />
+                  </a>
+
+                  <div className="flex flex-col gap-2 p-4 lg:p-6">
+                    <span className="text-sm text-gray-400">
+                      March 15, 2021
+                    </span>
+
+                    <h2 className="text-xl font-bold text-gray-800">
+                      <a
+                        href="#"
+                        className="transition duration-100 hover:text-indigo-500 active:text-indigo-600"
+                      >
+                        10 best smartphones for devs
+                      </a>
+                    </h2>
+
+                    <p className="text-gray-500">
+                      This is a section of some simple filler text, also known
+                      as placeholder text.
+                    </p>
+
+                    <div>
+                      <a
+                        href="#"
+                        className="font-semibold text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700"
+                      >
+                        Read more
+                      </a>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="flex flex-col items-center overflow-hidden rounded-lg border md:flex-row">
+                  <a
+                    href="#"
+                    className="group relative block h-48 w-full shrink-0 self-start overflow-hidden bg-gray-100 md:h-full md:w-32 lg:w-48"
+                  >
+                    <img
+                      src="https://images.unsplash.com/photo-1610465299996-30f240ac2b1c?auto=format&q=75&fit=crop&w=600"
+                      loading="lazy"
+                      alt="Photo by Martin Sanchez"
+                      className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+                    />
+                  </a>
+
+                  <div className="flex flex-col gap-2 p-4 lg:p-6">
+                    <span className="text-sm text-gray-400">
+                      January 27, 2021
+                    </span>
+
+                    <h2 className="text-xl font-bold text-gray-800">
+                      <a
+                        href="#"
+                        className="transition duration-100 hover:text-indigo-500 active:text-indigo-600"
+                      >
+                        8 High performance Notebooks
+                      </a>
+                    </h2>
+
+                    <p className="text-gray-500">
+                      This is a section of some simple filler text, also known
+                      as placeholder text.
+                    </p>
+
+                    <div>
+                      <a
+                        href="#"
+                        className="font-semibold text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700"
+                      >
+                        Read more
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-3">
+            <div className="flex justify-center lg:justify-center px-4 py-3 items-center gap-2 rounded-md border-2 border-gray-200 font-semibold text-blue-500 hover:text-white hover:bg-blue-500 hover:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:border-gray-700 dark:hover:border-blue-500">
+              <Link href="/static">すべての記事を見る</Link>
+            </div>
+          </div>
+        </div>
+        <div className="lg:col-span-1">
+          <Sidebar />
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
